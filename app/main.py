67bb07e6633389fea1bd0056feb7cb748528eba1
Maxim Rukoleev <maxprolife@icloud.com>
2023-02-13 15:50:33 +0300

    updated the main function

--- app/main.py
+++ app/main.py
@@ -21,7 +21,8 @@ def init_driver():
     chrome_options.add_argument('--disable-blink-features=AutomationControlled')
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option('useAutomationExtension', False)
-    chrome_options.add_argument(f'user-agent={generate_fake_ua()}')
+    chrome_options.add_argument('user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
+                                'like Gecko) Chrome/77.0.3865.90 Safari/537.36"')
     selenium_wire_options = {
         'addr': 'django',
         'proxies': {
@@ -39,11 +40,6 @@ def init_driver():
     return browser
 
 
-def generate_fake_ua() -> UserAgent:
-    ua = UserAgent()
-    return ua.random
-
-
 def get_keywords_list(queryList: list) -> list:
     keywords = []
     if not queryList:
@@ -57,12 +53,10 @@ def get_keywords_list(queryList: list) -> list:
 
 
 def get_position(domain_list: list, keywords: list) -> list:
-    # options = Options()
-    # options.page_load_strategy = 'normal'
-    # browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     browser = init_driver()
     href_dict = {}
     result = []
+    count_get = 0
     next_page_link = []
     for page in range(1, 4):
         count = 0
@@ -70,23 +64,27 @@ def get_position(domain_list: list, keywords: list) -> list:
             url = 'https://www.google.com/search?q=' + keyword if page < 3 else next_page_link[count]
             try:
                 browser.get(url)
+                time.sleep(randrange(2, 5))  # пауза между запросами, спасение от бана или капчи
+                count_get += 1
+                browser.get_screenshot_as_file(f"resource/{count_get}.png")
+                print("----------------")
+                print(count)
+                print("----------------")
                 if page == 2:
-                    print("----------------")
-                    print(count)
-                    print("----------------")
+                    browser.get_screenshot_as_file(f"resource/next_2page_link{count_get}.png")
                     next_page_link.append(
                         browser.find_element(By.XPATH, '//td[@class="d6cvqb BBwThe"]//a[@id="pnnext"]').get_attribute(
                             'href'))
+
                     browser.get(next_page_link[count])
                     print(f"Count: {count} url: {next_page_link[count]}")
 
                 elif page == 3:
-                    print("----------------")
-                    print(count)
-                    print("----------------")
+                    browser.get_screenshot_as_file(f"resource/next_3page_link{count_get}.png")
                     next_page_link[count] = browser.find_element(By.XPATH,
                                                                  '//td[@class="d6cvqb BBwThe"]//a[@id="pnnext"]').get_attribute(
                         'href')  # заменил ссылку на вторую страницу ссылкой на третью
+
                     browser.get(next_page_link[count])
                     print(f"Count: {count} url: {next_page_link[count]}")
 
@@ -96,7 +94,6 @@ def get_position(domain_list: list, keywords: list) -> list:
                 logging.exception(ex)
 
             count += 1
-            # href_dict = check_for_webcache(href_dict, DOMAIN)# словарь с сайтами без рекламы
             print(f"Page is: {page}")
             for key, value in href_dict.items():
                 print(f"{key} : {value}")
@@ -125,7 +122,8 @@ def check_for_webcache(href_dict: dict) -> dict:
 def main():
     query_list = [
         'кухни зов ',
-        'купить кухни'
+        'купить кухни',
+        'популярные crm системы'
     ]
 
     domain_list = [
@@ -136,7 +134,8 @@ def main():
         'zovrus.ru',
         'leroymerlin.ru',
         'mebelveb.ru',
-        'mas-mebel.ru'
+        'mas-mebel.ru',
+        'mebelpik.ru'
     ]
 
     result = get_position(domain_list, get_keywords_list(query_list))
